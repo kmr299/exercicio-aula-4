55ca27fa05a97cf5e446f7d6d1f2dc6af5e41deb
Kauã <kaua@eagoravai.com.br>
2018-01-02 20:02:32 -0200

    add async method

--- ExercicioAula4/ExercicioAula4/Program.cs
+++ ExercicioAula4/ExercicioAula4/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using Dapper;
 using System.IO;
-using System.Collections.Generic;
 using System.Data.SQLite;
+using System.Threading.Tasks;
 
 namespace ExercicioAula4
 {
@@ -11,13 +11,19 @@ namespace ExercicioAula4
     {
         static void Main(string[] args)
         {
+            var t = MainAsync();
+            t.Wait();
+        }
+
+        static async Task MainAsync()
+        {
             Console.WriteLine("Listagem de músicas");
             Console.WriteLine("====================================================");
 
             while (true)
             {
                 Console.WriteLine("1 - Listar artistas");
-                Console.WriteLine("2 - Listar albuns");
+                Console.WriteLine("2 - Listar artistas e albuns");
                 Console.WriteLine("3 - Listar gêneros");
                 Console.WriteLine("4 - Listar músicas");
 
@@ -28,7 +34,7 @@ namespace ExercicioAula4
                 {
 
                     var databasePath = string.Format("Data Source={0};Version=3;", Path.Combine(Directory.GetCurrentDirectory(), "App_Data", "database.db"));
-                    
+
                     using (var conn = new SQLiteConnection(databasePath))
                     {
                         switch (opcao)
@@ -37,13 +43,10 @@ namespace ExercicioAula4
 
                                 Console.Clear();
 
-                                var result = conn.Query<dynamic>("SELECT ArtistId as Id, Name as Name FROM artists");
+                                var result = await conn.QueryAsync<dynamic>("SELECT ArtistId as Id, Name as Name FROM artists");
 
-                                foreach(dynamic i in result)
-                                {
+                                foreach (dynamic i in result)
                                     Console.WriteLine("| {0} | {1} |", i.Id, i.Name);
-                                }
-
 
                                 break;
                         }
